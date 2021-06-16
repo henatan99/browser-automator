8442d4d16364997a096a02211240916118785148
Henok <henatan99@gmail.com>
2021-06-16 05:26:36 +0300

    Add simple broweser automation code

--- index.rb
+++ index.rb
@@ -1,3 +1,12 @@
 require 'watir'
-browser = Watir::Browser.new
+require 'webdrivers'
 
+browser = Watir::Browser.start 'watir.com/examples/simple_form.html'
+
+header = browser.element(id: 'user')
+header.present? # => true
+header.text == 'Add user' # => true
+header.attribute('data-test') == 'header' # => true
+header.click
+
+browser.close
