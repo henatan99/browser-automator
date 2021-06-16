97ab2d2f15fb1e3b81ae56f596bd8536c9ba39f1
Henok <henatan99@gmail.com>
2021-06-16 08:10:06 +0300

    Add auto login and click link functionalites

--- index.rb
+++ index.rb
@@ -1,12 +1,30 @@
 require 'watir'
 require 'webdrivers'
 
-browser = Watir::Browser.start 'watir.com/examples/simple_form.html'
+account = File.open("account.txt")
+lines = account.readlines
+email = lines[0]
+puts email
+password = lines[1]
+puts password
 
-header = browser.element(id: 'user')
-header.present? # => true
-header.text == 'Add user' # => true
-header.attribute('data-test') == 'header' # => true
-header.click
+browser = Watir::Browser.new
+browser.goto('https://dashboard.microverse.org/review_requests')
+
+browser.text_field(id: '1-email').set email
+browser.text_field(type: 'password').set password
+browser.button(type: 'submit').click
+browser.link(:text =>"Review Requests 2.0").click
+
+loop do
+    begin
+      sleep(1)
+      puts browser.span(class:'review-request-announcement-header').text 
+    rescue
+      puts "Nothing"
+    end
+end
+
+sleep(120)
 
 browser.close
