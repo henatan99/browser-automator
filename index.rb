cf43eb4c1d8f575919a27913f597708719fe715e
Henok <henatan99@gmail.com>
2021-06-18 14:26:46 +0300

    Add refresh and Error handling features

--- index.rb
+++ index.rb
@@ -3,30 +3,54 @@ require 'webdrivers'
 
 account = File.open("account.txt")
 lines = account.readlines
-email = lines[0]
-puts email
-password = lines[1]
-puts password
+EMAIL = lines[0]
+puts EMAIL
+PASSWORD = lines[1]
+puts PASSWORD
 
-browser = Watir::Browser.new
-browser.goto('https://dashboard.microverse.org/review_requests')
+def browser_auto
+  client = Selenium::WebDriver::Remote::Http::Default.new
+  client.read_timeout = 300 # seconds
+  driver = Selenium::WebDriver.for :chrome,http_client: client
 
-browser.text_field(id: '1-email').set email
-browser.text_field(type: 'password').set password
-browser.button(type: 'submit').click
-browser.link(:text =>"Review Requests 2.0").click
+  browser = Watir::Browser.new
+  browser.goto('https://dashboard.microverse.org/review_requests')
 
-loop do
-    begin
-      sleep(1)
-      target_text = browser.span(class:'review-request-announcement-header').text 
-      browser.button(:text => "Claim").click if browser.button(:text => "Claim")
-      puts target_text 
-    rescue
-      puts "Nothing"
+  browser.text_field(id: '1-email').set EMAIL
+  browser.text_field(type: 'password').set PASSWORD
+  browser.button(type: 'submit').click
+  browser.link(:text =>"Review Requests 2.0").click
+
+  counter = 0
+  loop do
+      begin
+        sleep(0.2)
+        target_text = browser.span(class:'review-request-announcement-header').text 
+        browser.button(:text => 'Claim').click if target_text != 'No Available Reviews'
+        puts target_text 
+      rescue
+        puts "Nothing"
+      end
+      if (counter >= 50) 
+        browser.refresh 
+        counter = 0
+      else 
+        counter += 1
+      end 
+  end
+
+  sleep(5)
+
+  browser.close
+end 
+
+read_timeouts = 0
+begin 
+  browser_auto
+  rescue Net::ReadTimeout
+    STDOUT.puts 'Net::ReadTimeout during browser launch. Trying to launch the browser again'
+    read_timeouts += 1
+    unless read_timeouts > 20
+      browser_auto
     end
-end
-
-sleep(120)
-
-browser.close
+end 
